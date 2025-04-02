0848aab35f5c6ea48785240fbd673f3f70ce6810
FranJJL05 <fjimlop182@g.educaand.es>
2025-04-02 11:11:47 +0000

    creacion de la clase media ejer4

---
+++ UD6-Herencia/Relación 12- Arraylist/Ejercicio4/media.java
@@ -0,0 +1,20 @@
+package Ejercicio4;
+
+import java.util.HashSet;
+
+public class media {
+    public static void main(String[] args) {
+        HashSet<Integer> numeros = new HashSet<>();
+
+        /*Esto es para crear conjuntos 
+         * numeros es aquello que queremos guardar
+        */
+
+        numeros.add(4);
+        numeros.add(3);
+        numeros.add(2);
+        numeros.add(1);
+        numeros.add(5);
+        
+    }
+}
