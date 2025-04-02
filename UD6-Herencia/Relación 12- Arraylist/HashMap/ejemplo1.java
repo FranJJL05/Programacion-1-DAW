f701c1f8ff6d0d6cb4a3849a2ad9cbe588211ec2
FranJJL05 <fjimlop182@g.educaand.es>
2025-04-02 12:21:12 +0000

    continuacion ejemplo1 Hashset

--- UD6-Herencia/Relación 12- Arraylist/HashMap/ejemplo1.java
+++ UD6-Herencia/Relación 12- Arraylist/HashMap/ejemplo1.java
@@ -4,16 +4,27 @@ import java.util.HashMap;
 
 public class ejemplo1 {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        map.put("Elon", 21);
-        map.put("Jeff", 22);
-        map.put("Bill", 48);
-        map.put("Warren", null);
+        HashMap<String, Integer> edades = new HashMap<String, Integer>();
+        Integer edad;
+
+        edades.put("Elon", 21);
+        edades.put("Jeff", 22);
+        edades.put("Bill", 48);
+        edades.put("Warren", null);
         
-        for (String key: map.keySet())
+        /*Para acceder a solo una de ellas*/
+        edad = edades.get("Elon");
+        System.out.println("La edad de Elon es: " + edad);
+
+        /*Para recorrer todas las claves y te muestre el valor de cada una, es un acceso recorriendo el diccionario */
+        for (String clave: edades.keySet())
         {
-           Integer value = map.get(key);
-           System.out.println(key + " --> " + value);
+           Integer valor = edades.get(clave);
+           System.out.println(clave + " --> " + valor);
         }   
     }
 }
+
+/*ctrl + d para modificar un comando en varias lineas a la misma vez
+ * ctrl + alt + flechas para seleccionar lineas y modificar lo mismo que haya en esas lineas 
+ */
