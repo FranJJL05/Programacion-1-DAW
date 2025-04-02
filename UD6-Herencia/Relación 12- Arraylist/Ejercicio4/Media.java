4490913f6a57b9ebb27d7d5662d5e01796819672
FranJJL05 <fjimlop182@g.educaand.es>
2025-04-02 11:25:20 +0000

    clase Tipo comida ejer 5 y actualizacion y acabado del ejercicio 4 clase Media

--- UD6-Herencia/Relación 12- Arraylist/Ejercicio4/Media.java
+++ UD6-Herencia/Relación 12- Arraylist/Ejercicio4/Media.java
@@ -2,7 +2,7 @@ package Ejercicio4;
 
 import java.util.HashSet;
 
-public class media {
+public class Media {
     public static void main(String[] args) {
         HashSet<Integer> numeros = new HashSet<>();
 
@@ -10,11 +10,19 @@ public class media {
          * numeros es aquello que queremos guardar
         */
 
+        int suma = 0;
+
         numeros.add(4);
         numeros.add(3);
         numeros.add(2);
         numeros.add(1);
         numeros.add(5);
-        
+
+        for (Integer n : numeros) { /*En la variable n va a ir guardando los numeros que vea en cada pasada */
+            suma = suma + n;   
+        }
+
+        System.out.println("La suma de los números del conjunto es: " + suma);
+
     }
 }
