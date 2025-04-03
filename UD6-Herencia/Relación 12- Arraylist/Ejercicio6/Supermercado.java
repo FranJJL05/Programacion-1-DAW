be5e42292d56363b748ac77e771fcc6def3ed90a
FranJJL05 <fjimlop182@g.educaand.es>
2025-04-03 12:15:29 +0000

    cambio sobre precio1

--- UD6-Herencia/Relación 12- Arraylist/Ejercicio6/Supermercado.java
+++ UD6-Herencia/Relación 12- Arraylist/Ejercicio6/Supermercado.java
@@ -41,10 +41,10 @@ public class Supermercado {
         System.out.println("\n");
         System.out.printf("%-15s %-10s %-11s %-10s\n", "Producto", "Precio", "Cantidad", "Subtotal");
         System.out.println("-------------------------------------------------");
-        
+
         for (Compra compra : carro) {
             String producto1 = compra.getProducto();
-            double precio1 = productos.get(compra.getProducto());
+            double precio1 = productos.get(producto1);
             int cantidad1 = compra.getCantidad(); // Cambié a int, ya que es cantidad de productos
             double subtotal = precio1 * cantidad1;
 
