cd3a0b65d52e960b74e5dfaabd4e9a1ae38db6df
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-03 10:28:46 +0000

    ejer 8 relacion

--- Recuperacion_Programacion/ejer7/Pizza.java
+++ Recuperacion_Programacion/ejer7/Pizza.java
@@ -4,10 +4,11 @@ public class Pizza {
     private String tamaño;
     private String tipo;
     private String estado = "pedida";
-    private int totalPedidas = 0, totalServidas = 0;
+    private int totalPedidas = 0;
+    private int totalServidas = 0;
 
 
-    public Pizza(String tamaño, String tipo, int totalPedidas) {
+    public Pizza(String tamaño, String tipo) {
         this.tamaño = tamaño;
         this.tipo = tipo;
         this.totalPedidas++;
@@ -18,6 +19,7 @@ public class Pizza {
             estado = "servida";
             System.out.println("La pizza está servida");
             totalServidas = totalServidas++;
+            System.out.println("El total de pizzas servidas es de " + totalServidas       );
         }else{
             System.out.println("La pizza ya se ha servido");
         }
