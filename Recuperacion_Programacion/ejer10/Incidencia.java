f272965f0fffc7d09f3b6d0caf0df55c3fdca477
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-04 08:27:37 +0000

    ejer10

---
+++ Recuperacion_Programacion/ejer10/Incidencia.java
@@ -0,0 +1,33 @@
+package ejer10;
+
+public class Incidencia {
+    private int numPuesto;
+    private String estado = "Pendiente";
+    private String fallo;
+    private static int pendiente = 0;
+    private String solucion = " ";
+
+
+    public Incidencia(int numPuesto, String fallo) {
+        this.numPuesto = numPuesto;
+        this.fallo = fallo;
+        pendiente ++;
+    }
+
+    public void resuelve(String solucion1){
+        estado = "Resuelta";
+        pendiente --;
+        solucion = solucion + "-" + solucion1;
+    }
+
+    public static int getPendiente() {
+        return pendiente;
+    }
+
+
+    @Override
+    public String toString(){
+        return "Puesto: " + numPuesto + " - " + fallo + " - " + estado + solucion;
+    }
+
+}
