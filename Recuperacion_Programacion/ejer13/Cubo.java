ad73e26ec2ba922593d24a58ef81e09159495866
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-04 15:06:19 +0000

    ejer13

--- Recuperacion_Programacion/ejer13/Cubo.java
+++ Recuperacion_Programacion/ejer13/Cubo.java
@@ -23,7 +23,7 @@ public class Cubo {
    
    public void llenar(double litros){
         litrosMomento += litros;
-        if (litros + litrosMomento > litrosMomento) {
+        if (litrosMomento > litrosMomento) {
             litrosMomento = capacidad;
             System.out.println("El cubo está lleno, no caben más de " + capacidad);
         }
--- Recuperacion_Programacion/ejer13/ProgramaCubo.java
+++ Recuperacion_Programacion/ejer13/ProgramaCubo.java
@@ -1,5 +1,18 @@
 package ejer13;
 
 public class ProgramaCubo {
-    
+     public static void main(String[] args) {
+        Cubo miCubo = new Cubo(10.0, 0.0);
+
+        System.out.println("Litros en el cubo al inicio: " + miCubo.getLitrosMomento());
+
+        miCubo.llenar(5.0);
+        System.out.println("Litros en el cubo después de llenar con 5 litros: " + miCubo.getLitrosMomento());
+
+        miCubo.llenar(10.0);
+        System.out.println("Litros en el cubo después de intentar llenar con 10 litros: " + miCubo.getLitrosMomento());
+
+        miCubo.vaciar();
+        System.out.println("Litros en el cubo después de vaciar: " + miCubo.getLitrosMomento());
+    }
 }
