f1629eae68c663b03e1e9a98eeff3fceae1191c1
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-04 07:16:06 +0000

    ejer9

---
+++ Recuperacion_Programacion/ejer9/ProgramaTiempo.java
@@ -0,0 +1,16 @@
+package ejer9;
+
+public class ProgramaTiempo{
+    public static void main(String[] args) {
+        Tiempo t1 = new Tiempo(1,30,40);
+        Tiempo t2 = new Tiempo(0, 25, 20);
+
+        Tiempo suma = t1.suma(t2);
+        System.out.println("Suma: " + suma);
+
+        Tiempo resta = t1.resta(t2);
+        System.out.println("Resta: " + resta);
+
+    }
+
+}
---
+++ Recuperacion_Programacion/ejer9/Tiempo.java
@@ -0,0 +1,64 @@
+package ejer9;
+
+public class Tiempo {
+    private int horas, minutos, segundos;
+
+
+    public Tiempo(int horas, int minutos, int segundos) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    void Normalizar() {
+        while (segundos >= 60) {
+            segundos -= 60;
+            minutos++;
+        }
+        while (minutos >= 60) {
+            minutos -= 60;
+            horas++;
+        }
+    }
+
+    public Tiempo suma(Tiempo otro){
+        int TotalSegundos = this.segundos + otro.segundos;
+        int TotalHoras = this.horas + otro.horas;
+        int TotalMinutos = this.minutos + otro.minutos;
+
+        Tiempo resultado = new Tiempo(0, 0, TotalSegundos);
+        resultado.minutos += TotalMinutos;
+        resultado.horas += TotalHoras;
+        resultado.Normalizar();
+
+        return resultado;
+    }
+
+    public Tiempo resta(Tiempo otro) {
+        int TotalSegundo = this.segundos - otro.segundos;
+        int TotalMinutos = this.minutos - otro.minutos;
+        int TotalHoras = this.horas - otro.horas;
+
+        if (TotalSegundo < 0) {
+            TotalSegundo += 60;
+            TotalMinutos--;
+        }
+
+        if (TotalMinutos < 0) {
+            TotalMinutos += 60;
+            TotalHoras--;
+        }
+
+        Tiempo resultado = new Tiempo(0, 0, TotalSegundo);
+        resultado.minutos += TotalMinutos;
+        resultado.horas += TotalHoras;
+        resultado.Normalizar();
+
+        return resultado;
+    }
+
+    @Override
+    public String toString(){
+        return horas + "h " + minutos + "m " + segundos + "s";
+    }
+}
