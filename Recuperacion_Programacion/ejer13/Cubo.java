e7ff6d1615d9a562b8e07cf871570e35371b1582
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-04 11:45:19 +0000

    ejer13

--- Recuperacion_Programacion/ejer13/Cubo.java
+++ Recuperacion_Programacion/ejer13/Cubo.java
@@ -1,9 +1,31 @@
 package ejer13;
 
 public class Cubo {
-    private double cantLitros;
-    private double capMax;
+    private double capacidad;
     private double litrosMomento;
 
-    
+    public Cubo(double capacidad, double litrosMomento){
+        this.capacidad = capacidad;
+        this.litrosMomento = 0;
+    }
+
+   public double getLitrosMomento() {
+       return litrosMomento;
+   }
+
+   public void setLitrosMomento(double litrosMomento) {
+       this.litrosMomento = litrosMomento;
+   }
+   
+   public void vaciar(){
+        litrosMomento = 0;
+   }
+   
+   public void llenar(double litros){
+        litrosMomento += litros;
+        if (litros + litrosMomento > litrosMomento) {
+            litrosMomento = capacidad;
+            System.out.println("El cubo está lleno, no caben más de " + capacidad);
+        }
+   }
 }
