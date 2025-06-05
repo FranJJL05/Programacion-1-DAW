590399e832b4ed49d1f3d2bc3af896256eb96338
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-05 08:12:21 +0000

    ejer2

---
+++ Recuperacion_Programacion/Relacion10/ejer2/Articulo.java
@@ -0,0 +1,68 @@
+package Relacion10.ejer2;
+
+public class Articulo{
+    private String codigo;
+    private String descripcion;
+    private double precioCompra;
+    private double precioVenta;
+    private int stock;
+
+    
+    public Articulo(String codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+
+    public void setPrecioCompra(double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+
+    public int getStock() {
+        return stock;
+    }
+
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    
+
+}
---
+++ Recuperacion_Programacion/Relacion10/ejer2/ProgramaArticulo.java
@@ -0,0 +1,11 @@
+package Relacion10.ejer2;
+
+public class ProgramaArticulo {
+    public static void main(String[] args) {
+        Articulo[] articulos = new Articulo[3];
+        
+    }
+
+
+
+}
