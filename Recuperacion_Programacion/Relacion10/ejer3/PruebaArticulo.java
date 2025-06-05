a3a07084da1b9485a968bbe95a3332c795b6b744
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-05 10:28:29 +0000

    ejer3 relacion10

---
+++ Recuperacion_Programacion/Relacion10/ejer3/PruebaArticulo.java
@@ -0,0 +1,36 @@
+package Relacion10.ejer3;
+
+import Relacion10.ejer2.Articulo;
+
+public class PruebaArticulo {
+    
+    int tamaño;
+    Articulo[] articulos;
+
+    public PruebaArticulo(int tamaño){
+        articulos = new Articulo[tamaño];
+    }
+
+    public boolean almacena(Articulo articulo1){
+        for (int i = 0; i < articulos.length; i++) {
+            if (articulos[i] == null) {
+                articulos[i] = articulo1;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < articulos.length; i++) {
+            if (articulos[i] == null) {
+                result += "Articulo " + (i + 1) + ":\n" +
+                            articulos[i].toString() + ":\n" +
+                            "--------------------------\n";
+            }
+        }
+        return result;
+    }
+}
