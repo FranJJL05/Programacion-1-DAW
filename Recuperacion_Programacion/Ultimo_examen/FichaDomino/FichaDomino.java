2309efc3d82089612a91eff7a6a82651625eadfc
FranJJL05 <fjimlop182@g.educaand.es>
2025-06-11 11:57:53 +0000

    ejer ficha domino examen

---
+++ Recuperacion_Programacion/Ultimo_examen/FichaDomino/FichaDomino.java
@@ -0,0 +1,42 @@
+package Ultimo_examen.FichaDomino;
+
+public class FichaDomino {
+    private int lado1;
+    private int lado2;
+
+    public FichaDomino(int lado1, int lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    public String encaja(FichaDomino otraFicha) {
+        String resultado = " ";
+        if (lado1 == otraFicha.lado1 || lado1 == otraFicha.lado2 || lado2 == otraFicha.lado1 || lado2 == otraFicha.lado2) {
+            resultado += "true";
+        } else {
+            resultado += "false";
+        }
+
+        return resultado;
+    }
+
+    @Override
+    public String toString() {
+        String l1;
+        String l2;
+
+        if (lado1 == 0) {
+            l1 = " ";
+        } else {
+            l1 = String.valueOf(lado1);
+        }
+
+        if (lado2 == 0) {
+            l2 = " ";
+        } else {
+            l2 = String.valueOf(lado2);
+        }
+
+        return "[" + l1 + "|" + l2 + "]";
+    }
+}
